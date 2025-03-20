dbea59ba229e3d74ad8dc98ad066a111414b02b2
simeonreusch <mail@simeonreusch.com>
2025-03-20 10:55:06 +0100

    remove legacy iport from __init__

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,37 +1,63 @@
-import os
+import daiquiri.core.env as env
 
-from . import ADDITIONAL_APPS, BASE_DIR, DJANGO_APPS
+from . import ADDITIONAL_APPS, DJANGO_APPS
 
-SITE_IDENTIFIER = 'example.com'
-SITE_TITLE = 'example.com'
-SITE_DESCRIPTION = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
-SITE_LICENSE = 'CC0'
-SITE_CREATOR = 'Anna Admin'
+SENDFILE_BACKEND = env.get("SENDFILE_BACKEND")
+SENDFILE_ROOT = env.get("SENDFILE_ROOT")
+SENDFILE_URL = env.get("SENDFILE_URL")
+
+SITE_IDENTIFIER = "example.com"
+SITE_TITLE = "example.com"
+SITE_DESCRIPTION = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+SITE_LICENSE = "CC0"
+SITE_CREATOR = "Anna Admin"
 SITE_CONTACT = {
-    'name': 'Anna Admin',
-    'address': 'Example Road 1',
-    'email': 'admin@example.com',
-    'telephone': '+01 234 56789'
+    "name": "EScience Team at AIP",
+    "address": "An der Sternwarte 16, 14482 Potsdam",
+    "email": "escience@aip.de",
 }
-SITE_PUBLISHER = 'At vero eos et accusam'
-SITE_CREATED = '2019-01-01'
-SITE_UPDATED = '2019-04-01'
+SITE_PUBLISHER = "Leibniz Institute for Astrophysics Potsdam (AIP)"
+SITE_CREATED = "2019-01-01"
+SITE_UPDATED = "2019-04-01"
 
-INSTALLED_APPS = DJANGO_APPS + [
-    'theme',
-    'daiquiri.auth',
-    'daiquiri.conesearch',
-    'daiquiri.contact',
-    'daiquiri.core',
-    'daiquiri.datalink',
-    'daiquiri.files',
-    'daiquiri.jobs',
-    'daiquiri.metadata',
-    'daiquiri.oai',
-    'daiquiri.query',
-    'daiquiri.registry',
-    'daiquiri.serve',
-    'daiquiri.stats',
-    'daiquiri.tap',
-    'daiquiri.uws',
-] + ADDITIONAL_APPS
+INSTALLED_APPS = (
+    DJANGO_APPS
+    + [
+        "theme",
+        "daiquiri.auth",
+        "daiquiri.conesearch",
+        "daiquiri.contact",
+        "daiquiri.core",
+        "daiquiri.datalink",
+        "daiquiri.files",
+        "daiquiri.jobs",
+        "daiquiri.metadata",
+        "daiquiri.oai",
+        "daiquiri.query",
+        "daiquiri.registry",
+        "daiquiri.serve",
+        "daiquiri.stats",
+        "daiquiri.tap",
+        "daiquiri.uws",
+    ]
+    + ADDITIONAL_APPS
+)
+
+PASSWORD_HASHERS = [
+    "daiquiri.core.hashers.CrypdSHA512PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
+
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+ACCOUNT_PASSWORD_MIN_LENGTH = 8
+
+ACCOUNT_LOGOUT_ON_GET = True
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 7
+ACCOUNT_EMAIL_VERIFICATION = env.get("ACCOUNT_EMAIL_VERIFICATION", "mandatory")
+
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+
+AUTH_SIGNUP = True
+AUTH_TERMS_OF_USE = True
--- config/urls.py
+++ config/urls.py
@@ -1,38 +1,44 @@
-from django.urls import include, path
+from daiquiri.core.views import home
+from daiquiri.files.views import FileView
 from django.contrib import admin
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from django.views.i18n import JavaScriptCatalog
 
-from daiquiri.core.views import home
-
-
 urlpatterns = [
-    path('', home, name='home'),
-
-    path('accounts/', include('daiquiri.auth.urls_accounts')),
-
-    path('auth/', include('daiquiri.auth.urls_auth', namespace='auth')),
-    path('conesearch/', include('daiquiri.conesearch.urls', namespace='conesearch')),
-    path('contact/', include('daiquiri.contact.urls', namespace='contact')),
-    path('datalink/', include('daiquiri.datalink.urls', namespace='datalink')),
-    path('files/', include('daiquiri.files.urls', namespace='files')),
-    path('metadata/', include('daiquiri.metadata.urls', namespace='metadata')),
-    path('oai/', include('daiquiri.oai.urls', namespace='oai')),
-    path('registry/', include('daiquiri.registry.urls', namespace='registry')),
-    path('serve/', include('daiquiri.serve.urls', namespace='serve')),
-    path('stats/', include('daiquiri.stats.urls', namespace='stats')),
-    path('query/', include('daiquiri.query.urls', namespace='query')),
-    path('tap/', include('daiquiri.tap.urls', namespace='tap')),
-    path('uws/', include('daiquiri.uws.urls', namespace='uws')),
-
-    path('robots.txt', TemplateView.as_view(template_name='site/robots.txt', content_type='text/plain'), name='robots'),
-    path('layout/', TemplateView.as_view(template_name='wordpress/layout.html'), name='layout'),
-    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-
-    path('admin/', admin.site.urls),
+    path("", home, name="home"),
+    path("accounts/", include("daiquiri.auth.urls_accounts")),
+    path("auth/", include("daiquiri.auth.urls_auth", namespace="auth")),
+    path("conesearch/", include("daiquiri.conesearch.urls", namespace="conesearch")),
+    path("contact/", include("daiquiri.contact.urls", namespace="contact")),
+    path("datalink/", include("daiquiri.datalink.urls", namespace="datalink")),
+    path("files/", include("daiquiri.files.urls", namespace="files")),
+    path("metadata/", include("daiquiri.metadata.urls", namespace="metadata")),
+    path("oai/", include("daiquiri.oai.urls", namespace="oai")),
+    path("registry/", include("daiquiri.registry.urls", namespace="registry")),
+    path("serve/", include("daiquiri.serve.urls", namespace="serve")),
+    path("stats/", include("daiquiri.stats.urls", namespace="stats")),
+    path("query/", include("daiquiri.query.urls", namespace="query")),
+    path("tap/", include("daiquiri.tap.urls", namespace="tap")),
+    path("uws/", include("daiquiri.uws.urls", namespace="uws")),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="site/robots.txt", content_type="text/plain"
+        ),
+        name="robots",
+    ),
+    path(
+        "layout/",
+        TemplateView.as_view(template_name="wordpress/layout.html"),
+        name="layout",
+    ),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
+    path("admin/", admin.site.urls),
+    re_path(r"cms/(?P<file_path>.*)$", FileView.as_view(root="cms"), name="cms"),
 ]
 
-handler400 = 'daiquiri.core.views.bad_request'
-handler403 = 'daiquiri.core.views.forbidden'
-handler404 = 'daiquiri.core.views.not_found'
-handler500 = 'daiquiri.core.views.internal_server_error'
+handler400 = "daiquiri.core.views.bad_request"
+handler403 = "daiquiri.core.views.forbidden"
+handler404 = "daiquiri.core.views.not_found"
+handler500 = "daiquiri.core.views.internal_server_error"
